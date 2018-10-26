a77834138049752cb1a99aa07833df0c69812584
Tal <ty4273@bs.amalnet.k12.il>
2018-10-26 15:46:21 +0300

    Initial commit

--- app/src/main/java/com/example/tal/alertdialogrgb/CreditsActivity.java
+++ app/src/main/java/com/example/tal/alertdialogrgb/CreditsActivity.java
@@ -2,6 +2,7 @@ package com.example.tal.alertdialogrgb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class CreditsActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home)
+            this.finish();
+        return super.onOptionsItemSelected(item);
     }
 }
